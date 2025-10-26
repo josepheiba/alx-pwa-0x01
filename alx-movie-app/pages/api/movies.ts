61e9592376b55180f8a8a992087aafb40308e37d
Joseph Eiba <josepheiba@icloud.com>
2025-10-26 16:53:18 +0100

    ok

--- alx-movie-app/pages/api/movies.ts
+++ alx-movie-app/pages/api/movies.ts
@@ -1,5 +1,25 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
+interface Book {
+  cover_i?: number;
+  key?: string;
+  title?: string;
+  first_publish_year?: number;
+  author_name?: string[];
+}
+
+interface ApiResponse {
+  docs?: Book[];
+}
+
+interface Movie {
+  id: string | number;
+  title: string;
+  image: string;
+  year: number | string;
+  author: string;
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -12,13 +32,13 @@ export default async function handler(
 
     if (!response.ok) throw new Error("Failed to fetch");
 
-    const data = await response.json();
+    const data: ApiResponse = await response.json();
 
     // Transform the data to look like movie data
-    const movies = (data.docs || [])
-      .filter((book: any) => book.cover_i)
+    const movies: Movie[] = (data.docs || [])
+      .filter((book: Book) => book.cover_i)
       .slice(0, 12)
-      .map((book: any, idx: number) => ({
+      .map((book: Book, idx: number) => ({
         id: book.key || idx,
         title: book.title || "Unknown",
         image: `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`,
